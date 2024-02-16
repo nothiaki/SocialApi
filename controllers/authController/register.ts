7e54e414b6e93042188ec47b710e080d7abb9b64
NotHiaki <celsohenrique367@gmail.com>
2024-02-16 20:36:42 -0300

    feat: register user

--- controllers/authController/register.ts
+++ controllers/authController/register.ts
@@ -1,11 +1,57 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const prisma = new PrismaClient();
 
-export async function register(req: Request, res: Response) {
-    const data = req.body;
-    const user = await prisma.user.create({ data });
+interface ReqUser {
+  username: string,
+  email: string,
+  password: string
+}
 
-    return res.status(201).json({ user });
+export async function register(req: Request, res: Response) {
+  const data: ReqUser = req.body;
+
+  const userSchema = z.object({
+    username: z.string().min(1),
+    email: z.string().email().min(1),
+    password: z.string().min(4).max(50)
+  });
+
+  const userIsValid = userSchema.safeParse(data);
+
+  if (!userIsValid.success) {
+    return res.status(500).json({
+      message: userIsValid.error.issues[0].message
+    });
+  };
+
+  const usernameExist = await prisma.user.findUnique({
+    where: {
+      username: data.username
+    }
+  });
+
+  if (usernameExist) {
+    return res.status(409).json({
+      message: 'Username already exist.'
+    });
+  };
+
+  const emailExist = await prisma.user.findUnique({
+    where: {
+      email: data.email
+    }
+  });
+
+  if (emailExist) {
+    return res.status(409).json({
+      message: 'Email already exist.'
+    });
+  };
+
+  const user = await prisma.user.create({ data });
+
+  return res.status(201).json({ user });
 }
